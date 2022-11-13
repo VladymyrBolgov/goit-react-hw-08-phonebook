0abdeaab476661a3d80707a54d74f790cd67ed8d
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-13 12:23:16 +0200

    gf

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { authReducer } from "./Auth/slice";
 import { persistedReducer } from './contactsSlice';
 import { filterReducer } from "./filterSlice";
 
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
     filter: filterReducer,
     auth: authReducer,
-  },
+ },
 });
+
