3b0e15f787de9f3e0126053593edcca3e55aca6d
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-13 19:38:15 +0200

    пар

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -15,3 +15,5 @@ export const useAuth = () => {
         user,
     };
 };
+
+
--- src/redux/Auth/selectors.js
+++ src/redux/Auth/selectors.js
@@ -1,3 +1,14 @@
+/*const getIsLoggedIn = state => state.userAuth.isLoggedIn;
+const getUserName = state => state.userAuth.user.name;
+const getUserEmail = state => state.userAuth.user.email; 
+
+const userAuthSelectors = {
+    getIsLoggedIn,
+    getUserName,
+    getUserEmail
+};
+export default userAuthSelectors;*/
+
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
