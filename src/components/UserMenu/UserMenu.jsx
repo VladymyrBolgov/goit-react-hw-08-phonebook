bf7d50504f35fc3c1fd3c4772d270b38d019fb6a
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-13 21:27:26 +0200

    rt

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,17 @@
 
 import { useAuth } from 'hooks'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import {logOut} from 'redux/Auth/operations'
 
 export const UserMenu = () => {
-    const {user} = useAuth
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
   return (
     <div>
           <p>Welcome, {user.name}</p>
-          <button type='button'>Logout</button>
+          <button type='button' onClick={() => dispatch(logOut())}>Logout</button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import  App  from './components/App'
 import './index.css';
 import { Provider } from 'react-redux';
 import { store, } from './redux/store';
-//import { persistor } from './redux/store';
-//import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import {persistor} from './redux/store.jsx'
@@ -14,11 +12,11 @@ import {persistor} from './redux/store.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+ 
         <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
-        </PersistGate>
+      
     </Provider>
   </React.StrictMode>
  
