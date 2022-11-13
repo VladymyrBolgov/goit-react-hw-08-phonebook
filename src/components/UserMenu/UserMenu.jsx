5bfd31c6b491cab559a27714c4217fe9b6b9e1ee
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-13 19:41:50 +0200

    ол

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,26 +1,3 @@
-//import { useAuth } from 'hooks'
-/*import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
-import userAuthSelectors from 'redux/Auth/selectors';
-import userAuthOperations from 'redux/Auth/operations';
-
-export const UserMenu = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const email = useSelector(userAuthSelectors.getUserEmail);
-
-  const logout = () => {
-    dispatch(userAuthOperations.logOut())
-    navigate('/login')
-  }
-  return (
-    <div>
-          <p>Welcome, {email}</p>
-          <button type='button' onClick = { logout}>Logout</button>
-    </div>
-  )
-}*/
 
 import { useAuth } from 'hooks'
 import React from 'react'
--- src/redux/Auth/selectors.js
+++ src/redux/Auth/selectors.js
@@ -1,13 +1,13 @@
-/*const getIsLoggedIn = state => state.userAuth.isLoggedIn;
-const getUserName = state => state.userAuth.user.name;
-const getUserEmail = state => state.userAuth.user.email; 
+//const getIsLoggedIn = state => state.userAuth.isLoggedIn;
+//const getUserName = state => state.userAuth.user.name;
+//const getUserEmail = state => state.userAuth.user.email; 
 
-const userAuthSelectors = {
-    getIsLoggedIn,
-    getUserName,
-    getUserEmail
-};
-export default userAuthSelectors;*/
+//const userAuthSelectors = {
+  //  getIsLoggedIn,
+  //  getUserName,
+  //  getUserEmail
+//};
+//export default userAuthSelectors;
 
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user;
