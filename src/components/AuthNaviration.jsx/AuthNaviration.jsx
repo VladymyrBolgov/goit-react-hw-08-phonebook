896e9567dc5c0784ccc963e71679402cbc12de60
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-14 21:42:58 +0200

    nav

--- src/components/AuthNaviration.jsx/AuthNaviration.jsx
+++ src/components/AuthNaviration.jsx/AuthNaviration.jsx
@@ -1,20 +1,17 @@
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/Auth/selectors';
+import { Link } from './AuthNavigation.styled'
 
 
 export const AuthNavigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn)
   return (
     <>
-      <Link to="/" end>
-        Home
-      </Link>
-      <Link  to="/register">
-          Register    
-      </Link>
-      <Link  to="/login">
-        Log In    
-      </Link>
+      <Link to="/" end>Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">{isLoggedIn ? 'Log Out' : 'Log In'}</Link>
     </>
-  )
-}
+  );
+};
+
+export default AuthNavigation;
