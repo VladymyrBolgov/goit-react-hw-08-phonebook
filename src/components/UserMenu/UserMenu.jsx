8f10320579d9d5b7b45729b9b0f528cf3fe63f2c
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-14 02:14:02 +0200

    gj

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,14 +2,18 @@
 import { useAuth } from 'hooks'
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import {userAuthOperations} from 'redux/Auth/operations'
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
+  const navigate = useNavigate
 
   const logOut = () => {
     dispatch(userAuthOperations.logOut())
+    navigate('/login')
+
   }
 
   return (
